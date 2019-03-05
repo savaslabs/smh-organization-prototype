c3e9b5b82784a805b9b63b79ee75750bbf98f7a8
Anne Tomasevich <annetvich@gmail.com>
2019-03-05 18:54:39 -0500

    Add details to member sidebar when verified

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,21 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Nav from 'react-bootstrap/Nav';
 
-const Sidebar = ({ member, idVerified, accessRecords, onSelect }) => {
+const Sidebar = ({ member, idVerified, onSelect }) => {
   const image = member.profileImage ? member.profileImage : '/images/avatars/default.png';
   const tabTitle = idVerified ? 'Identification' : 'Verify Identification';
 
   return (
     <div className="member-sidebar col-md-3">
-      <img src={image} alt={member.name} className="member-avatar"/>
+      <div className="member-sidebar__image">
+        <img src={image} alt={member.name} className="member-avatar"/>
+        {idVerified &&
+        <p className="member-avatar__overlay mb-0">
+          <img src="/images/icons/star.png" alt="" />
+          ID Verified
+        </p>
+      }
+      </div>
       <div className="member-sidebar__nav">
-        <h1>{member.name}</h1>
+        <div className="member-sidebar__heading">
+          <h1>{member.name}</h1>
+          {idVerified &&
+          <p>{member.gender} <span className="mr-2 ml-2">·</span> Age {member.age}</p>
+          }
+        </div>
+
         <Nav defaultActiveKey="memberInfo" className="flex-column" onSelect={(active) => onSelect(active)}>
           <Nav.Link eventKey="memberInfo">Basic Information</Nav.Link>
           <Nav.Link eventKey="memberId">{tabTitle}</Nav.Link>
-          {accessRecords &&
+          {idVerified &&
             <div>
-              <Nav.Link eventKey="memberRecords">Records</Nav.Link>
+              <Nav.Link eventKey="memberRecords">Medical Data</Nav.Link>
               <Nav.Link eventKey="memberNotes">Notes</Nav.Link>
               <Nav.Link eventKey="memberLockbox">Lockbox</Nav.Link>
             </div>
@@ -30,7 +44,6 @@ const Sidebar = ({ member, idVerified, accessRecords, onSelect }) => {
 Sidebar.propTypes = {
   member: PropTypes.object.isRequired,
   idVerified: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-  accessRecords: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
   onSelect: PropTypes.func.isRequired
 };
 
