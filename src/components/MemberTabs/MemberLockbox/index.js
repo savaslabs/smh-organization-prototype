501fdb62c61019dd778526cd93efcfdac58e0bd9
Anne Tomasevich <annetvich@gmail.com>
2019-03-06 14:37:25 -0500

    Clean up notes and lockbox tabs

--- src/components/MemberTabs/MemberLockbox/index.js
+++ src/components/MemberTabs/MemberLockbox/index.js
@@ -5,7 +5,7 @@ import BasicInfo from '../MemberInfo/BasicInfo';
 
 const MemberLockbox = ({ member }) => (
   <div>
-    <h2>Lockbox</h2>
+    <h2 className="member-tabs__title">Lockbox</h2>
     <BasicInfo member={member} />
   </div>
 );
